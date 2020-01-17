31a7828fea610bedb52f94135b755b29996a8a00
Haydn Adams <haydn@beyondpapavalley.com>
2020-01-17 10:43:40 -0500

    added a sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `AeroBarrier Pros`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `AeroBarrier Pros`,
+    siteUrl: `https://aerobarrierpros.com`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,6 +14,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-sitemap`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
