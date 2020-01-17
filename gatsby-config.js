758536b6c5a5d00a532beb0cf6ba3769a81aca3e
Haydn Adams <haydn@beyondpapavalley.com>
2020-01-17 10:30:17 -0500

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,10 @@
 module.exports = {
-  pathPrefix: `/gatsby-creative`,
   siteMetadata: {
-    title: `AeroBarrier Pros`,
-    description: ``,
-    author: ``,
-    siteUrl: `aerobarrierpros.com`,
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
   plugins: [
-    
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -21,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `AeroBarrier Pros`,
+        name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
@@ -29,20 +26,6 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
-    },
-    `gatsby-plugin-sass`,
-    
-
-
-
-    
-    {
-      resolve: 'gatsby-plugin-web-font-loader',
-      options: {
-        google: {
-          families: ['Merriweather:400,300,300italic,400italic,700,700italic', 'Merriweather+Sans:400,700']
-        }
-      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
---
+++ public/styles-e23215ce736d5774444e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{ImdL:function(n,o,p){},XjQp:function(n,o){}}]);
+//# sourceMappingURL=styles-e23215ce736d5774444e.js.map
