966abef9c46c68a614b875ec42f98ba1039b0a45
Haydn Adams <haydn@beyondpapavalley.com>
2020-01-17 10:46:09 -0500

    updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
     title: `AeroBarrier Pros`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `AeroBarrier Pros`,
-    siteUrl: `https://aerobarrierpros.com`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -14,7 +13,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-plugin-sitemap`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,11 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import rich from "../images/rich.jpg"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1 className="text-center">Ouch. We know how it feels.</h1>
-    <p className="text-center"><img src={rich} alt="Rich" className="img-fluid" style={{ maxWidth: `400px` }} /></p>
+    
 
     <p className="text-center">You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
